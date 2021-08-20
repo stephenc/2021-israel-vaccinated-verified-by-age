e4a5aeaf6400fb19831f88304e8cfad5b9656bd6
Stephen Connolly <stephen.alan.connolly@gmail.com>
2021-08-20 16:55:31 +0100

    Start collecting

---
+++ scrape.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+	alias sha256sum=gsha256sum
+fi
+
+export SCRAPE_DATE="$(TZ=UTC printf '%(%Y-%m-%d_%H.%M.%S)TZ\n')"
+
+if [ ! -d data ]
+then
+	mkdir -p data
+fi
+
+curl -fsSL 'https://datadashboardapi.health.gov.il/api/queries/_batch' \
+    -H 'Accept: application/json' \
+    --compressed \
+    -H 'Content-Type: application/json' \
+    --data-raw '{"requests":[{"id":"0","queryName":"vaccinatedVerifiedByAge","single":false,"parameters":{}}]}' \
+    > tmp.json
+
+export CONTENT_HASH=$(sha256sum tmp.json | sed -e 's/ .*$//')
+
+if sha256sum data/*.json | grep -F "${CONTENT_HASH}" >> /dev/null 
+then
+	rm -f tmp.json
+else
+	mv -f tmp.json "data/scrape-${SCRAPE_DATE}.json"
+	git add "data/scrape-${SCRAPE_DATE}.json"
+fi
